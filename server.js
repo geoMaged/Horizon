6fed1714c9db58a2516b2938904819b836726f2a
George Maged <g.maged327@gmail.com>
2020-06-24 23:38:44 +0200

    Version 1.0.0

--- server.js
+++ server.js
@@ -332,14 +332,11 @@ function authenticateToken(req,res,next) {
 //Serve Static assets if in production 
 if(process.env.NODE_ENV === 'production'){
   //Set static folder
-  app.use(express.static('frontend/build'));
-
-  
-
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
 }
-app.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
-});
 
 const port = process.env.PORT || 3001;
 
