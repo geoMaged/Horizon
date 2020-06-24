071c3b1b6f806da20c2e1a276ba15764b5241a19
George Maged <g.maged327@gmail.com>
2020-06-24 23:25:44 +0200

    Version 1.0.0

--- server.js
+++ server.js
@@ -334,11 +334,12 @@ if(process.env.NODE_ENV === 'production'){
   //Set static folder
   app.use(express.static('frontend/build'));
 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
-  });
+  
 
 }
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+});
 
 const port = process.env.PORT || 3001;
 
